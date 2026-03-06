859196f47935b8a5be17a7d4c58fe742729db780
Om Tiwari <tiwariom22082005@gmail.com>
2026-03-06 21:09:13 +0530

    restore redis connection

--- redisClient.js
+++ redisClient.js
@@ -1,5 +1,13 @@
-module.exports = {
-    set: async () => {
-        return "OK";
-    }
-};
+const redis = require("redis");
+
+const client = redis.createClient({
+  url: process.env.REDIS_URL
+});
+
+client.on("error", (err) => console.log("Redis Client Error", err));
+
+(async () => {
+  await client.connect();
+})();
+
+module.exports = client;
